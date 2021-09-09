34af252c518c13d89bf2577242d33d69db0aed5a
Aliresa <aliresa.1998@gmail.com>
2021-09-09 19:22:52 +0430

    Add new family member - csrf token have problem !

--- app/client/src/views/NewFriend.js
+++ app/client/src/views/NewFriend.js
@@ -1,15 +1,23 @@
 import React, {useState, useEffect} from "react"
+import axios from 'axios';
 import Breadcrumb from "../components/breadcrumb/Breadcrumb";
 
 import Field from "../components/form/Field";
 import ProfilePic from "../components/profilepic/ProfilePic";
-
+import { pledge } from '../../../controller/pledgeController';
 export default function NewFriend() {
 
     const [formErr, setFormErr] = useState({});
+    const [inputFormPic, setinputFormPic] = useState({
+        profile_photo:"",
+    });
     const [inputForm, setInputForm] = useState({
-        email_address: "",
-        password: ""
+        first_name:"",
+        last_name:"",
+        profile_photo:null,
+        phone_number:"",
+        email: "",
+        relation: ""
     });
     
     const [box, setBox] = useState(setBox)
@@ -27,24 +35,58 @@ export default function NewFriend() {
 
     
     const handleSubmit = async event => {
-        event.preventDefault()        
+        event.preventDefault()
+        console.log(inputFormPic)
+        const response = await pledge.CreateFamilyMember(
+          inputForm["first_name"],
+          inputForm["last_name"],
+          inputFormPic["profile_photo"],
+          inputForm["phone_number"],
+          inputForm["email"],
+          inputForm["relation"],
+        )
+
+        console.log("response", response);
+
+        //console.log(inputForm)  
+
     }
+
+    
+    const handleFileChosen = (file) => {
+        setinputFormPic({
+          "profile_photo": file
+        })
+        
+    };
 
     return (
         <>
-            <p onClick={boxStatus} id="addnewfriendBtn" className="highlight" style={{cursor: "pointer"}}>Add</p>
+            <button onClick={boxStatus} id="addnewfriendBtn" className="highlight" style={{cursor: "pointer",backgroundColor:"white",border:"none"}}>Add</button>
             <section id="addnewfriendModel" className={`boxModel ${box?'show':''}`} >
                 <div className="inner">
                 <Breadcrumb onExit={onExit} current="Add Friends & Family" altTitle="Add" exit={true} exitReq={true} exitMode="outer" />
                 <form onSubmit={handleSubmit} style={{marginTop: "20px"}}>
                     <ProfilePic text="Upload profile image" style="noborder" />
+
+                    <input type="file"
+                        title="profile_photo"required={true} //onChange={handleInput}
+                        onChange={e => handleFileChosen(e.target.files[0])}
+                        accept="image/png, image/jpeg"/>
+
+               
                     <div className="card">
-                        <Field mode={true} nomargin={true} valid={formErr.non_field_errors?false:true} message={formErr.non_field_errors} required={true} handleInput={handleInput} value={inputForm} hideTitle={true} title="First Name" type="string" />
-                        <Field mode={true} nomargin={true} valid={formErr.non_field_errors?false:true} message={formErr.non_field_errors} required={true} handleInput={handleInput} value={inputForm} hideTitle={true} title="Last Name" type="string" />
+                        <Field mode={true} nomargin={true} valid={formErr.non_field_errors?false:true} message={formErr.non_field_errors} required={true} onChange={handleInput} value={inputForm.first_name} hideTitle={true} title="First Name" type="string" />
+                        
+                        <Field mode={true} nomargin={true} valid={formErr.non_field_errors?false:true} message={formErr.non_field_errors} required={true} onChange={handleInput} value={inputForm.last_name} hideTitle={true} title="Last Name" type="string" />
                     </div>
-                    <Field mode={true} nomargin={true} valid={formErr.non_field_errors?false:true} message={formErr.non_field_errors} required={true} handleInput={handleInput} value={inputForm} hideTitle={true} title="Phone Number" type="string" />
-                    <Field mode={true} nomargin={true} valid={formErr.non_field_errors?false:true} message={formErr.non_field_errors} required={true} handleInput={handleInput} value={inputForm} hideTitle={true} title="Email Address" type="string" />
-                    <Field mode={true} nomargin={true} valid={formErr.non_field_errors?false:true} message={formErr.non_field_errors} required={true} handleInput={handleInput} value={inputForm} title="Last Name" type="select" options={['Father', 'Mother', 'Sibling', 'Grandparent', 'Friend']} />
+                    <Field mode={true} nomargin={true} valid={formErr.non_field_errors?false:true} message={formErr.non_field_errors} required={true} onChange={handleInput} value={inputForm.phone_number} hideTitle={true} title="Phone Number" type="string" />
+
+                    <Field mode={true} nomargin={true} valid={formErr.non_field_errors?false:true} message={formErr.non_field_errors} required={true} onChange={handleInput} value={inputForm.email} hideTitle={true} title="Email" type="string" />
+
+                    <Field mode={true} nomargin={true} valid={formErr.non_field_errors?false:true} message={formErr.non_field_errors} required={true} onChange={handleInput} value={inputForm.relation} title="relation" type="select" options={['Father', 'Mother', 'Sibling', 'Grandparent', 'Friend']} />
+
+                    <input onClick={handleSubmit} className="submit" type="submit" value="Create" />
                 </form>
                 </div>
             </section>
--- app/controller/pledgeController.js
+++ app/controller/pledgeController.js
@@ -1,5 +1,5 @@
 import { account } from "./accountController";
-
+import axios from "axios";
 
 const createPledge = async (amount, interest_rate, term, monthly_payment, repayment_source, message) => {
     
@@ -39,8 +39,74 @@ const createPledge = async (amount, interest_rate, term, monthly_payment, repaym
   
   }
   
+  const CreateFamilyMember = async (first_name, last_name, profile_photo, phone_number, email, relation) => {
 
+  var myHeaders = new Headers();
+  myHeaders.append("accept", "application/json");
+  myHeaders.append("Content-Type", "application/json");
+  const auth = await account.authorised();
+  myHeaders.append("X-CSRFToken", auth.csrf)
+ 
+    const image = {
+      "profile_photo": profile_photo,
+    }
+    const data = {
+        "first_name": first_name,
+        "last_name": last_name,
+        "phone_number": phone_number,
+        "email": email,
+        "relation": "",
+    }
+    var raw = ({
+      image : profile_photo,
+      data : data
+    });
+
+    
+
+    console.log(raw)
+    raw = JSON.stringify(raw)
+
+
+
+    let req = new Request(
+      'https://papp.rastava.com/pledge/CreateFamilyMember/', 
+      {
+        mode: 'cors',
+        credentials: 'include',
+        method: 'POST',
+        body: raw,
+        headers: myHeaders
+      }
+    );
+
+    console.log(req)
+
+    const response = await fetch(req)
+    const json = await response.json();
+    return {
+      json: json.data,
+      status: json.status,
+      message: json.message
+    };
+/*
+        const auth = await account.authorised();
+        console.log(auth)
+        const config = {
+            headers: { 'content-type': 'multipart/form-data' , "X-CSRFToken" :auth.csrf }
+        }
+        axios.post('https://papp.rastava.com/pledge/CreateFamilyMember/',inputForm,config)
+        .then(function (response) {
+            // handle success
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error.data);
+        });
+*/
+
+  }
 
   export const pledge = {
-    createPledge
+    createPledge,CreateFamilyMember
   }
